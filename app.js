b987f21c6e20831052d396a22c3723f673099321
FacereRerum <hrtalia.osi@gmail.com>
2024-05-10 19:07:04 +0700

    may

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
-const express = require('express');
+const express = require("express");
 const bodyParser = require('body-parser');
-const path = require('path');
-const usersRouter = require('./routes/users');
-const gamesRouter = require('./routes/games');
-const categoriesRouter = require('./routes/categories');
-
-const connectToDatabase = require('./database/connect');
-const cors = require('./middlewares/cors');
+const path = require("path");
+const usersRouter = require("./routes/users");
+const gamesRouter = require("./routes/games");
+const categoriesRouter = require("./routes/categories");
+const connectToDatabase = require("./database/connect");
 
 const app = express();
 const PORT = 3000;
@@ -14,12 +12,13 @@ const PORT = 3000;
 connectToDatabase();
 
 app.use(
-  cors, 
   bodyParser.json(),
-  express.static(path.join(__dirname, 'public')),
-  usersRouter, 
-  gamesRouter, 
+  express.static(path.join(__dirname, "public")),
+  usersRouter,
+  gamesRouter,
   categoriesRouter
 );
 
-app.listen(PORT);
+app.listen(PORT, () => {
+  console.log(`Server is running at PORT http://localhost:${PORT}`);
+}); 
--- routes/game.js
+++ routes/game.js
@@ -1,10 +1,44 @@
-// Файл routes/games.js
+const gamesRouter = require("express").Router();
 
-const gamesRouter = require('express').Router();
+const {
+  findAllGames,
+  createGame,
+  findGameById,
+  updateGame,
+  deleteGame,
+  checkEmptyFields,
+  checkIfCategoriesAvaliable,
+  checkIfUsersAreSafe,
+  checkIsGameExists,
+} = require("../middlewares/games");
+const {
+  sendAllGames,
+  sendGameCreated,
+  sendGameById,
+  sendGameUpdated,
+  sendGameDeleted,
+} = require("../controllers/games");
 
-const findAllGames = require('../middlewares/games');
-const sendAllGames = require('../controllers/games');
-
-gamesRouter.get('/games', findAllGames, sendAllGames);
+gamesRouter.get("/games", findAllGames, sendAllGames);
+gamesRouter.get("/games/:id", findGameById, sendGameById);
+gamesRouter.post(
+  "/games",
+  findAllGames,
+  checkIsGameExists,
+  checkIfCategoriesAvaliable,
+  checkEmptyFields,
+  createGame,
+  sendGameCreated
+);
+gamesRouter.delete("/games/:id", findGameById, deleteGame, sendGameDeleted);
+gamesRouter.put(
+  "/games/:id",
+  findGameById,
+  checkIfUsersAreSafe,
+  checkIfCategoriesAvaliable,
+  checkEmptyFields,
+  updateGame,
+  sendGameUpdated
+);
 
 module.exports = gamesRouter;
