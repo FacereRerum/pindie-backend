a7dc386287e6ecacc2a8792ac70d521fc5bb22c3
FacereRerum <149412021+FacereRerum@users.noreply.github.com>
2024-05-29 23:11:34 +0800

    ma

--- app/api/config.js
+++ app/api/config.js
@@ -1,5 +1,4 @@
-export const BASE_URL = "https://pindie-haiks1.nomoredomainswork.ru/api";
-// export const BASE_URL = "https://rm-pindie-back.nomoredomainswork.ru/api";
+export const BASE_URL = "https://backendmay.nomorepartiesco.ru/api";
 
 export const endpoints = {
   games: `${BASE_URL}/games`,
